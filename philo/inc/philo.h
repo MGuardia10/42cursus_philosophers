3baa0603a19ca45f05361e5f80a6202af51a68b2
Miguel Guardia <mike.guardia0309@gmail.com>
2023-12-23 13:39:04 +0100

    src and inc dirs created && Makefile working && .gitignore added

---
+++ philo/inc/philo.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mguardia <mguardia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/23 12:27:08 by mguardia          #+#    #+#             */
+/*   Updated: 2023/12/23 13:02:32 by mguardia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>	// threads
+# include <string.h>	// memset
+# include <stdio.h>		// printf
+# include <stdlib.h>	// malloc, free
+# include <unistd.h>	// write, usleep
+# include <sys/time.h>	// gettimeofday
+
+
+// # include <limits.h>
+// # include <signal.h>
+// # include <stdarg.h>
+
+#endif
---
+++ philo/src/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mguardia <mguardia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/23 12:25:18 by mguardia          #+#    #+#             */
+/*   Updated: 2023/12/23 13:03:29 by mguardia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+// void	leaks(void)
+// {
+// 	system("leaks -q philo");
+// }
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	
+	// atexit(leaks);
+	// printf("HOLAAAA\n");
+	return (0);
+}
