c858bef63a1323ac2e8b883c045a166901d4f9c7
Miguel Guardia <mike.guardia0309@gmail.com>
2024-01-25 20:04:04 +0100

    deadlock in supervisor fixed && one philo case done

--- philo/src/simulation.c
+++ philo/src/simulation.c
@@ -6,7 +6,7 @@
 /*   By: mguardia <mguardia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 10:21:54 by mguardia          #+#    #+#             */
-/*   Updated: 2024/01/24 11:12:16 by mguardia         ###   ########.fr       */
+/*   Updated: 2024/01/25 19:55:51 by mguardia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,10 @@
 
 bool	is_dead(t_philo philo)
 {
-	size_t	t_last_meal;
-	size_t	t_die;
 	size_t	dif;
 
-	pthread_mutex_lock(&philo.philo_mtx);
-	t_last_meal = philo.last_meal_time;
-	t_die = philo.data->time_die;
-	pthread_mutex_unlock(&philo.philo_mtx);
-	dif = get_time(MILISECONDS) - t_last_meal;
-	if (dif > t_die)
+	dif = get_time(MILISECONDS) - philo.last_meal_time;
+	if (dif > philo.data->time_die)
 		return (true);
 	return (false);
 }
@@ -44,35 +38,36 @@ static void	*philo_routine(void *data)
 	philo = (t_philo *)data;
 	// esperar que todos los hilos sean creados
 	wait_all_threads(philo->data);
-	// establecer tiempo en ms de la ultima comida y aumentar en +1 el numero de philos corriendo
-	set_size_t(philo->philo_mtx, &philo->last_meal_time, get_time(MILISECONDS));
-	set_size_t(philo->data->table_mtx, &philo->data->n_philos_running, \
-		get_size_t(philo->data->table_mtx, &philo->data->n_philos_running) + 1);
+	philo->last_meal_time = get_time(MILISECONDS);
+	increment_n_philos(philo->data);
 	// Si solo hay un philo:
-	if (get_size_t(philo->data->table_mtx, &philo->data->n_philos) == 1)
+	if (philo->data->n_philos == 1)
 		return (one_philo_routine(philo));
 	// caso contrario: comenzar bucle
-
-	//TODO
+	while (is_simulation_finish(philo->data) == false)
+	{
+		// TODO
+		usleep(100);
+	}
 	return (NULL);
 }
 
 void	*supervisor_routine(void *data)
 {
-	t_all	*table;
+	t_all			*table;
 	unsigned int	i;
 
 	table = (t_all *)data;
-	while (all_philos_running(table->table_mtx, table) == false)
+	while (all_philos_running(table->control_mtx, table) == false)
 		;
 	while (is_simulation_finish(table) == false)
 	{
 		i = -1;
-		while (++i < table->n_philos)
+		while (++i < table->n_philos && is_simulation_finish(table) == false)
 		{
 			if (is_dead(table->philos[i]) == true)
 			{
-				set_bool(table->table_mtx, &table->end_sim, true);
+				set_bool(table->control_mtx, &table->end_sim, true);
 				print_action(DEAD, table, table->philos[i]);
 			}
 		}
@@ -85,20 +80,21 @@ int	start_simulation(t_all *data)
 	unsigned int	i;
 
 	i = 0;
+	if (pthread_create(&data->supervisor, NULL, supervisor_routine, data) != 0)
+		return (printf("%s"THREAD_ERROR"%s\n", RED, RES), 1);
 	while (i < data->n_philos)
 	{
-		if (pthread_create(&data->philos[i].thread_id, NULL, philo_routine, &data->philos[i]) != 0)
+		if (pthread_create(&data->philos[i].thread_id, NULL, philo_routine, \
+														&data->philos[i]) != 0)
 			return (printf("%s"THREAD_ERROR"%s\n", RED, RES), 1);
 		i++;
 	}
-	if (pthread_create(&data->supervisor, NULL, supervisor_routine, data) != 0)
-		return (printf("%s"THREAD_ERROR"%s\n", RED, RES), 1);
 	data->start_time = get_time(MILISECONDS);
-	set_bool(data->table_mtx, &data->start_sim, true);
+	set_bool(data->control_mtx, &data->start_sim, true);
 	i = -1;
 	while (++i < data->n_philos)
 		pthread_join(data->philos[i].thread_id, NULL);
-	set_bool(data->table_mtx, &data->end_sim, true);
+	set_bool(data->control_mtx, &data->end_sim, true);
 	pthread_join(data->supervisor, NULL);
 	return (0);
 }
--- philo/src/syncro_utils.c
+++ philo/src/syncro_utils.c
@@ -6,35 +6,29 @@
 /*   By: mguardia <mguardia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 09:10:50 by mguardia          #+#    #+#             */
-/*   Updated: 2024/01/24 11:04:11 by mguardia         ###   ########.fr       */
+/*   Updated: 2024/01/25 19:55:12 by mguardia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../inc/philo.h"
+#include "../inc/philo.h"
 
 void	wait_all_threads(t_all *data)
 {
-	while (get_bool(data->start_mtx, &data->start_sim) == false)
+	while (get_bool(data->control_mtx, &data->start_sim) == false)
 		;
 }
 
 bool	all_philos_running(t_mtx mutex, t_all *table)
 {
-	bool	all_running;
+	size_t	philos_running;
 
 	pthread_mutex_lock(&mutex);
-	// printf("philos running: %zu\n", table->n_philos_running);
-	// printf("philos total: %zu\n", table->n_philos);
-	if (table->n_philos_running == table->n_philos)
-		all_running = true;
-	else
-		all_running = false;
+	philos_running = table->all_philos_running;
 	pthread_mutex_unlock(&mutex);
-	return (all_running);
+	return (philos_running);
 }
 
 bool	is_simulation_finish(t_all *data)
 {
-	return (get_bool(data->table_mtx, &data->end_sim));
+	return (get_bool(data->control_mtx, &data->end_sim));
 }
-
