f9ffc6233c2f67f0d6f6af8c68515531fcd400c2
Miguel Guardia <mike.guardia0309@gmail.com>
2024-01-28 16:44:45 +0100

    TODO list removed

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mguardia <mguardia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 12:25:18 by mguardia          #+#    #+#             */
-/*   Updated: 2024/01/28 16:33:49 by mguardia         ###   ########.fr       */
+/*   Updated: 2024/01/28 16:43:17 by mguardia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,24 +36,3 @@ int	main(int argc, char **argv)
 	safe_exit(&table);
 	return (EXIT_SUCCESS);
 }
-
-/*
-TODO:
-	- parsear los argumentos OK
-	- gestion de errores de argumentos OK
-	- iniciar mutex fork OK
-	- iniciar threads philos OK
-	- asignar tenedores a los filosofos OK
-	- mirar como se gestiona el tiempo gettimeofday() OK
-	- gestionar acciones: coger tenedor, comer, dormir, pensar, morir
-		timestamp_in_ms - philo[X] has taken a fork. OK
- 		timestamp_in_ms - philo[X] is eating. OK
- 		timestamp_in_ms - philo[X] is sleeping. OK
- 		timestamp_in_ms - philo[X] is thinking. OK
- 		timestamp_in_ms - philo[X] died. OK
-	- Revisar casos limite como que solo haya un philosofo, falte arg opcional,
-	  no mas de 10ms entre mensaje y muerte de un philo (subject)... OK
-	- norminette OK
-	- liberar memoria, cerrar hilos y destruir mutex al acabar el programa OK
-	- revisar leaks en fallos previos a la simulacion PENDING
-*/ 
